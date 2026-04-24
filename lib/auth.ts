980dd732cf958797397aa3a8dacd53a46487a17b
nandan.nagane <nandan.nagane@hiddenbrains.in>
2026-04-24 16:47:04 +0530

    stateless auth error

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,14 @@
 import { betterAuth } from "better-auth";
-
+import { memoryAdapter } from "better-auth/adapters/memory";
 export const auth = betterAuth({
+  // database: memoryAdapter(),
+
   session: {
     cookieCache: {
       enabled: true,
-      maxAge: 7 * 24 * 60 * 60, // 7 days cache duration
-      strategy: "jwt", // can be "jwt" or "compact"
-      refreshCache: true, // Enable stateless refresh
+      maxAge: 7 * 24 * 60 * 60,
+      strategy: "jwt",
+      refreshCache: true,
     },
   },
   account: {
@@ -18,6 +20,7 @@ export const auth = betterAuth({
     twitter: {
       clientId: process.env.TWITTER_CLIENT_ID as string,
       clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+      scope: ["users.read", "tweet.read", "offline.access"],
     },
   },
 });

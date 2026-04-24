7a1d1d74a1f2e966678e2dae95f03dab1192a7e6
nandan.nagane <nandan.nagane@hiddenbrains.in>
2026-04-24 15:10:10 +0530

    Init commit

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import Form from "@/components/login-form";
+
+export default function Login() {
+  return (
+    <div className="flex flex-1 justify-center items-center">
+      <Form className="border p-7" />
+    </div>
+  );
+}
